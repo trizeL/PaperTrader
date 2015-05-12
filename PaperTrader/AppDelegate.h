93e1bfaa74e2fa1a8e6a292b5a07481bdf4fa274
Brian Fruin <bpfruin@gmail.com>
2015-05-11 20:27:14 -0500

    readded files

---
+++ PaperTrader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaperTrader
+//
+//  Created by Brian Fruin on 5/11/15.
+//  Copyright (c) 2015 Brian Fruin & Sean Petri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaperTrader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaperTrader
+//
+//  Created by Brian Fruin on 5/11/15.
+//  Copyright (c) 2015 Brian Fruin & Sean Petri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
